3bd698a96e889eb727fe2f74aac4a23c1be636cb
Rishe Shanthakumar <rishe.shanthakumar@gmail.com>
2026-06-06 22:20:52 +0200

    mongo db key

--- src/lib/db.js
+++ src/lib/db.js
@@ -5,7 +5,7 @@ import { env } from '$env/dynamic/private';
 // Node.js picks up broken IPv6-only DNS servers on some Windows setups; force reliable resolvers.
 dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
 
-const MONGODB_URI = env.MONGODB_URI;
+const MONGODB_URI = env.DB_URI;
 
 let clientPromise = null;
 
